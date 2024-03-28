50bbab2e9f9427a6b9ce85e1114328b925824679
Cawa <Cawa@CAWAKA>
2024-03-28 22:45:41 +0300

    добавлен класс для фильтрации результатов

---
+++ webSkin/Models/SearchingClass.cs
@@ -0,0 +1,10 @@
+﻿namespace webSkin.Models
+{
+    public class SearchingClass
+    {
+        public decimal minPrice { get; set; }  
+        public decimal maxPrice { get; set; }
+        public string Type { get; set; }
+        public string Exterior { get; set; }
+    }
+}
