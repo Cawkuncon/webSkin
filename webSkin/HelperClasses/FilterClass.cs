d42667b8041b82a5427dcdb27801457f4a57dca4
Cawkuncon <homenet10@yandex.ru>
2024-04-02 14:44:57 +0300

    добавлена дополнительная фильтрация

--- webSkin/HelperClasses/FilterClass.cs
+++ webSkin/HelperClasses/FilterClass.cs
@@ -3,22 +3,42 @@ using webSkin.Models;
 
 namespace webSkin.HelperClasses
 {
-    public static class FilterClass
-    {
-        public static void GetFilterSkins(ref List<ResultItem> items, SearchingClass argsClass)
-        {
-            if (argsClass.Type != null)
-            {
-                items = items.Where(skin => skin.buff_type_localized_name == argsClass.Type).ToList();
-            }
-            if (argsClass.Exterior != null)
-            {
-                items = items.Where(skin => skin.buff_exterior_localized_name == argsClass.Exterior).ToList();
-            }
-            if (argsClass.minPrice != null && argsClass.maxPrice != null)
-            {
-                items = items.Where(skin => skin.buff_sell_min_price >= argsClass.minPrice && skin.buff_sell_min_price <= argsClass.maxPrice).ToList();
-            }
-        }
-    }
+	public static class FilterClass
+	{
+		public static void GetFilterSkins(ref List<ResultItem> items, SearchingClass argsClass)
+		{
+			if (argsClass.Type != null)
+			{
+				items = items.Where(skin => skin.buff_type_localized_name == argsClass.Type).ToList();
+			}
+			if (argsClass.Exterior != null)
+			{
+				items = items.Where(skin => skin.buff_exterior_localized_name == argsClass.Exterior).ToList();
+			}
+			if (argsClass.minPrice != null && argsClass.maxPrice != null)
+			{
+				items = items.Where(skin => skin.buff_sell_min_price >= argsClass.minPrice && skin.buff_sell_min_price <= argsClass.maxPrice).ToList();
+			}
+			if (argsClass.minBM != null && argsClass.maxBM != null)
+			{
+				items = items.Where(skin => skin.pricesBM >= argsClass.minBM && skin.pricesBM <= argsClass.maxBM).ToList();
+			}
+			if (argsClass.maxMB != null && argsClass.minMB != null)
+			{
+				items = items.Where(skin => skin.pricesMB >= argsClass.minMB && skin.pricesMB <= argsClass.maxMB).ToList();
+			}
+			if (argsClass.Sort != null)
+			{
+				if (argsClass.Sort == "order")
+				{
+					items.OrderBy(skin => skin.market_popularity_7d);
+				}
+				else if (argsClass.Sort == "orderDescending")
+				{
+					items.OrderByDescending(skin => skin.market_popularity_7d);
+				}
+				
+			}
+		}
+	}
 }
--- webSkin/Models/SearchingClass.cs
+++ webSkin/Models/SearchingClass.cs
@@ -2,9 +2,15 @@
 {
     public class SearchingClass
     {
-        public decimal? minPrice { get; set; }  
+        public decimal? minPrice { get; set; }
         public decimal? maxPrice { get; set; }
         public string Type { get; set; }
         public string Exterior { get; set; }
+        public string Sort { get; set; }
+        public decimal? minMB { get; set; }
+        public decimal? maxMB { get; set; }
+        public decimal? minBM { get; set; }
+        public decimal? maxBM { get; set; }
+
     }
 }
