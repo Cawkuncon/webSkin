58b9ce672ada0a9583216066d5a33beb4d446cf6
Cawkuncon <homenet10@yandex.ru>
2024-04-04 23:43:38 +0300

    убран футер, сделана пагинация

--- webSkin/Controllers/HomeController.cs
+++ webSkin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using ParseTry.Main;
@@ -9,46 +10,55 @@ using webSkins.Data;
 
 namespace webSkin.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ISkinRepositroy skinRepositroy;
-        private IMemoryCache cache;
-        private List<ResultItem> items;
-        private bool flagToResetFilter;
-        public HomeController(ILogger<HomeController> logger, ISkinRepositroy skinRepositroy, IMemoryCache memoryCache)
-        {
-            _logger = logger;
-            this.skinRepositroy = skinRepositroy;
-            cache = memoryCache;
-        }
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ISkinRepositroy skinRepositroy;
+		private IMemoryCache cache;
+		private List<ResultItem> items;
+		private bool flagToResetFilter;
+		private int pageSize = 20;
+		public HomeController(ILogger<HomeController> logger, ISkinRepositroy skinRepositroy, IMemoryCache memoryCache)
+		{
+			_logger = logger;
+			this.skinRepositroy = skinRepositroy;
+			cache = memoryCache;
+		}
 
-        public async Task<IActionResult> IndexAsync()
-        {
-            cache.TryGetValue("skins", out items);
-            if (items == null)
-            {
-                items = await skinRepositroy.GetAllSkinsAsync();
-                cache.Set("skins", items, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(20)));
-                flagToResetFilter = false;
-                cache.Set("flagToResetFilter", flagToResetFilter);
-			}
-            ViewBag.Skins = items;
-            return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> IndexAsync(SearchingClass argsClass, string Reset)
-        {
-            if (Reset != null)
-            {
+		public async Task<IActionResult> IndexAsync(int page = 1)
+		{
+			cache.TryGetValue("skins", out items);
+			if (items == null)
+			{
 				items = await skinRepositroy.GetAllSkinsAsync();
 				cache.Set("skins", items, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(20)));
 				flagToResetFilter = false;
 				cache.Set("flagToResetFilter", flagToResetFilter);
 			}
-            else
-            {
+			var count = items.Count;
+			items = items.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+			var pageViewModel = new PageViewModel(count, page, pageSize);
+			var indexViewModel = new IndexViewModel()
+			{
+				PageViewModel = pageViewModel,
+				Items = items
+			};
+			ViewBag.IndexViewModel = indexViewModel;
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> IndexAsync(SearchingClass argsClass, string Reset, int page = 1)
+		{
+			if (Reset != null)
+			{
+				items = await skinRepositroy.GetAllSkinsAsync();
+				cache.Set("skins", items, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(20)));
+				flagToResetFilter = false;
+				cache.Set("flagToResetFilter", flagToResetFilter);
+			}
+			else
+			{
 				cache.TryGetValue("skins", out items);
 				cache.TryGetValue("flagToResetFilter", out flagToResetFilter);
 				if (items == null || (items != null && flagToResetFilter))
@@ -60,19 +70,27 @@ namespace webSkin.Controllers
 				flagToResetFilter = true;
 				cache.Set("flagToResetFilter", flagToResetFilter);
 			}
-			ViewBag.Skins = items;
-            return View();
-        }
+			var count = items.Count;
+			items = items.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+			var pageViewModel = new PageViewModel(count, page, pageSize);
+			var indexViewModel = new IndexViewModel()
+			{
+				PageViewModel = pageViewModel,
+				Items = items
+			};
+			ViewBag.IndexViewModel = indexViewModel;
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
---
+++ webSkin/HelperClasses/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ParseTry.Main;
+
+namespace webSkin.HelperClasses
+{
+	public class IndexViewModel
+	{
+		public List<ResultItem> Items { get; set; }
+		public PageViewModel PageViewModel { get; set; }
+	}
+}
